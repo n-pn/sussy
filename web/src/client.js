62a9070134555d90f01bb0eeedae9dbc2eba0a56
Nipinium <nipinium@gmail.com>
2020-01-11 20:09:45 +0700

    WIP: fix styles

--- web/src/client.js
+++ web/src/client.js
@@ -1,6 +1,6 @@
 import * as sapper from '@sapper/app'
-import '../css/global.scss'
+import '../../src/global.scss'
 
 sapper.start({
-  target: document.querySelector('#sapper'),
+    target: document.querySelector('#sapper'),
 })
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -4,36 +4,36 @@ const sass = require('node-sass')
 const prepend_content = '@import "helpers";\n'
 
 module.exports = function(dir = __dirname) {
-  return {
-    preprocess: {
-      style: async ({ content, attributes, filename }) => {
-        const { type, lang } = attributes
-        if (type !== 'text/scss' && lang !== 'scss') return
-        content = prepend_content + content
+    return {
+        preprocess: {
+            style: async ({ content, attributes, filename }) => {
+                const { type, lang } = attributes
+                if (type !== 'text/scss' && lang !== 'scss') return
+                content = prepend_content + content
 
-        return new Promise((resolve, reject) => {
-          sass.render(
-            {
-              data: content,
-              sourceMap: 'style.css.map',
-              omitSourceMapUrl: true,
-              includePaths: [
-                path.dirname(filename),
-                path.join(dir, 'node_modules'),
-                path.resolve(__dirname, 'css'),
-              ],
-              outFile: 'x',
+                return new Promise((resolve, reject) => {
+                    sass.render(
+                        {
+                            data: content,
+                            sourceMap: 'style.css.map',
+                            omitSourceMapUrl: true,
+                            includePaths: [
+                                path.dirname(filename),
+                                path.join(dir, 'node_modules'),
+                                path.resolve(__dirname, '../src'),
+                            ],
+                            outFile: 'x',
+                        },
+                        (err, result) => {
+                            if (err) return reject(err)
+                            resolve({
+                                code: result.css.toString(),
+                                map: result.map.toString(),
+                            })
+                        }
+                    )
+                })
             },
-            (err, result) => {
-              if (err) return reject(err)
-              resolve({
-                code: result.css.toString(),
-                map: result.map.toString(),
-              })
-            }
-          )
-        })
-      },
-    },
-  }
+        },
+    }
 }
